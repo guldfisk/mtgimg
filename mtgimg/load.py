63f25206c566230b911f85ed8b2ef86f5b284b00
guldfisk <ce.guldfisk@gmail.com>
2018-06-10 03:02:13 +0200

    wauw

--- mtgimg/load.py
+++ mtgimg/load.py
@@ -15,6 +15,10 @@ from mtgimg.interface import ImageRequest, Imageable, ImageLoader, picturable
 from mtgimg import crop as image_crop
 
 
+IMAGE_SIZE = (745, 1040)
+CROPPED_IMAGE_SIZE = (560, 435)
+
+
 class TaskAwaiter(object):
 
 	def __init__(self):
@@ -24,10 +28,12 @@ class TaskAwaiter(object):
 	def get_condition(self, image_request: ImageRequest) -> t.Tuple[Condition, bool]:
 		with self._lock:
 			previous_condition = self._map.get(image_request, None)
+
 			if previous_condition is None:
 				condition = Condition()
 				self._map[image_request] = condition
 				return condition, False
+
 			return previous_condition, True
 
 
@@ -35,20 +41,27 @@ class ImageFetchException(Exception):
 	pass
 
 
-class _ImageProcessor(object):
-	_size = (745, 1040)
+class _ImageableProcessor(object):
+	_processing = TaskAwaiter()
 
 	@classmethod
-	def save_imageable(cls, image_request: ImageRequest, loader: ImageLoader, condition: Condition) -> Image.Image:
+	def _save_imageable(
+		cls,
+		image_request: ImageRequest,
+		size: t.Tuple[int, int],
+		loader: ImageLoader,
+		condition: Condition
+	) -> Image.Image:
+
 		image = image_request.pictured.get_image(
-			cls._size,
+			size,
 			loader,
 			image_request.back,
 			image_request.crop,
 		)
 
-		if image.size != cls._size:
-			image = image.resize(cls._size)
+		if image.size != size:
+			image = image.resize(size)
 
 		if not os.path.exists(image_request.dir_path):
 			os.makedirs(image_request.dir_path)
@@ -60,11 +73,32 @@ class _ImageProcessor(object):
 
 		return image
 
+	@classmethod
+	def get_image(cls, image_request: ImageRequest, loader: ImageLoader):
+		try:
+			return Loader.open_image(image_request.path)
+		except FileNotFoundError:
+			pass
+
+		condition, in_progress = cls._processing.get_condition(image_request)
+
+		if in_progress:
+			with condition:
+				condition.wait()
+
+			return Loader.open_image(image_request.path)
+
+		return cls._save_imageable(
+			image_request,
+			CROPPED_IMAGE_SIZE if image_request.crop else IMAGE_SIZE,
+			loader,
+			condition,
+		)
 
 
-class _Fetcher(_ImageProcessor):
+class _Fetcher(object):
 	_fetching = TaskAwaiter()
-	_size = (745, 1040)
+	_size = IMAGE_SIZE
 
 	@classmethod
 	def _fetch_image(cls, condition: Condition, image_request: ImageRequest):
@@ -120,7 +154,7 @@ class _Fetcher(_ImageProcessor):
 				condition.notify_all()
 
 	@classmethod
-	def get_image(cls, image_request: ImageRequest, loader: ImageLoader) -> Image.Image:
+	def get_image(cls, image_request: ImageRequest) -> Image.Image:
 		try:
 			return Loader.open_image(image_request.path)
 		except FileNotFoundError:
@@ -132,66 +166,69 @@ class _Fetcher(_ImageProcessor):
 			with condition:
 				condition.wait()
 		else:
-			if isinstance(image_request.pictured, Imageable):
-				return cls.save_imageable(
-					image_request,
-					loader,
-					condition,
-				)
 			cls._fetch_image(condition, image_request)
 
 		return Loader.open_image(image_request.path)
 
 
-class _Cropper(_ImageProcessor):
-	_size = (560, 435)
+class _Cropper(object):
+	_size = CROPPED_IMAGE_SIZE
 	_cropping = TaskAwaiter()
 
 	@classmethod
 	def _cropped_image(cls, condition: Condition, image: Image.Image, image_request: ImageRequest) -> Image.Image:
 		if not os.path.exists(image_request.dir_path):
 			os.makedirs(image_request.dir_path)
+
 		cropped_image = image_crop.crop(image, image_request)
 		cropped_image.save(image_request.path, image_request.extension)
+
 		with condition:
 			condition.notify_all()
+
 		return cropped_image
 
 	@classmethod
-	def cropped_image(cls, image_request: ImageRequest, loader: ImageLoader) -> Image.Image:
+	def cropped_image(cls, image_request: ImageRequest) -> Image.Image:
 		try:
 			return Loader.open_image(image_request.path)
 		except FileNotFoundError:
 			pass
 
 		condition, in_progress = cls._cropping.get_condition(image_request)
+
 		if in_progress:
 			with condition:
 				condition.wait()
 			return Loader.open_image(image_request.path)
 
-		if isinstance(image_request.pictured, Imageable):
-			return cls.save_imageable(
-				image_request,
-				loader,
-				condition,
-			)
-
 		return cls._cropped_image(
 			condition,
-			_Fetcher.get_image(image_request.cropped_as(False), loader),
+			_Fetcher.get_image(image_request.cropped_as(False)),
 			image_request,
 		)
 
 
 class Loader(ImageLoader):
 
-	def __init__(self, executor: t.Union[Executor, int] = None):
-		self._executor = (
-			executor
-			if executor is isinstance(executor, Executor) else
+	def __init__(
+		self,
+		printing_executor: t.Union[Executor, int] = None,
+		imageable_executor: t.Union[Executor, int] = None
+	):
+		self._printings_executor = (
+			printing_executor
+			if printing_executor is isinstance(printing_executor, Executor) else
 			ThreadPoolExecutor(
-				max_workers = executor if isinstance(executor, int) else 10
+				max_workers = printing_executor if isinstance(printing_executor, int) else 10
+			)
+		)
+
+		self._imageables_executor = (
+			imageable_executor
+			if imageable_executor is isinstance(imageable_executor, Executor) else
+			ThreadPoolExecutor(
+				max_workers = imageable_executor if isinstance(imageable_executor, int) else 10
 			)
 		)
 
@@ -209,55 +246,34 @@ class Loader(ImageLoader):
 			image_request
 		)
 
-		if _image_request.crop:
+		if isinstance(_image_request.pictured, Imageable):
 			return Promise.resolve(
-				self._executor.submit(
-					_Cropper.cropped_image,
+				self._imageables_executor.submit(
+					_ImageableProcessor.get_image,
 					_image_request,
 					self,
+				)
+			)
+
+		if _image_request.crop:
+			return Promise.resolve(
+				self._printings_executor.submit(
+					_Cropper.cropped_image,
+					_image_request,
 				)
 			)
 
 		else:
 			return Promise.resolve(
-				self._executor.submit(
+				self._printings_executor.submit(
 					_Fetcher.get_image,
 					_image_request,
-					self,
 				)
 			)
 
 	def get_default_image(self) -> Promise:
 		return Promise.resolve(
-			self._executor.submit(
+			self._printings_executor.submit(
 				lambda : Loader.open_image(paths.CARD_BACK_PATH)
 			)
 		)
-
-
-def test():
-	import time
-	from mtgorp.db import load
-
-	print(paths.IMAGES_PATH)
-
-	image_loader = Loader()
-	db = load.Loader.load()
-
-	# cardboard = db.cardboards['Time Spiral']
-	cardboard = db.cardboards['Fire // Ice']
-	# printing = cardboard.from_expansion('USG')
-	printing = cardboard.from_expansion('APC')
-	print(printing, cardboard.printings)
-	# printing_2 = cardboard.from_expansion('M14')
-
-	st = time.time()
-
-	images = [image_loader.get_image(printing, crop=True) for _ in range(10)]
-
-	found_images = Promise.all(images).get()
-
-	print('done', found_images, len(found_images), time.time() - st)
-
-if __name__ == '__main__':
-	test()
