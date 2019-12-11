9c924a38faba344e882d9839c7756011c0fdd32d
carl-emil hjort-trærup <ce.guldfisk@gmail.com>
2019-12-11 15:48:34 +0100

    ok

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@ import os
 from setuptools import setup
 
 setup(
-    name='mtgimg',
-    version='1.0',
-    packages=['mtgimg'],
-    package_data={
+    name = 'mtgimg',
+    version = '1.0',
+    packages = ['mtgimg'],
+    package_data = {
         'mtgimg': [
             os.path.join('cardback', 'cardback.png'),
             os.path.join('cardback', 'cardback_m.png'),
             os.path.join('cardback', 'cardback_t.png'),
         ],
     },
-    include_package_data=True,
-    install_requires=[
+    include_package_data = True,
+    install_requires = [
         'appdirs',
         'lazy-property',
         'mtgorp @ https://github.com/guldfisk/mtgorp/tarball/master#egg=mtgorp-1.0',
