459f496584df7a736084b0def5ad141c6ffd426a
guldfisk <ce.guldfisk@gmail.com>
2021-07-23 19:33:59 +0200

    ok

--- mtgimg/paths.py
+++ mtgimg/paths.py
@@ -5,7 +5,11 @@ from appdirs import AppDirs
 
 APP_DATA_PATH = AppDirs('mtgimg', 'mtgimg').user_data_dir
 
-IMAGES_PATH = os.path.join(APP_DATA_PATH, 'images')
+try:
+    with open(os.path.join(APP_DATA_PATH, 'imagepath.txt'), 'r') as f:
+        IMAGES_PATH = f.read().strip()
+except FileNotFoundError:
+    IMAGES_PATH = os.path.join(APP_DATA_PATH, 'images')
 
 CARD_BACK_DIRECTORY_PATH = os.path.join(
     os.path.dirname(os.path.realpath(__file__)),
